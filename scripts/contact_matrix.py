083bfe7048614f1f76f5872b781e99ffff2c309d
Dave <d.e.magno13@gmail.com>
2021-11-08 17:29:28 +0800

    Add contact matrix generator

---
+++ scripts/contact_matrix.py
@@ -0,0 +1,86 @@
+"""Generate contact matrices for different actions (quarantine levels)."""
+
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+
+
+class ContactMatrix:
+    """Contact matrix generator"""
+
+    def __init__(self) -> None:
+        self._filename = "Social_contact_matrices_Philippines.xlsx"
+        self._school = self._read_data("School")
+        self._work = self._read_data("Work")
+        self._other_location = self._read_data("other_location")
+        self._home = self._read_data("Home")
+
+    @property
+    def _full_path(self) -> Path:
+        return Path.joinpath(Path.cwd(), 'data/contacts', self._filename)
+
+    def _read_data(self, sheet_name: str = 'All_location') -> pd.DataFrame:
+        return pd.read_excel(
+            str(self._full_path),
+            sheet_name,
+            index_col="Ages"
+        )
+
+    def _return_zero(self, df: pd.DataFrame, mode: List[str]) -> pd.DataFrame:
+        _df = df.copy(deep=True)
+        _df[mode] = 0
+        _df.loc[mode] = 0
+        return _df
+
+    def _compute_contact(self, df: pd.DataFrame, prop: float, sd: float) -> pd.DataFrame:
+        return (prop - (prop * sd)) * df
+
+    def get_contact(
+        self,
+        home: bool = True,
+        prop_work: float = 1.0,
+        prop_school: float = 1.0,
+        prop_other: float = 1.0,
+        sd_work: float = 0.0,
+        sd_school: float = 0.0,
+        sd_other: float = 0.0,
+        home_quarantine: list = [],
+        not_allowed_work: list = [],
+        not_allowed_other: list = [],
+        not_allowed_school: list = []
+    ):
+
+        if home_quarantine:
+            self._school, self._work, self._other_location = (
+                self._return_zero(self._school, home_quarantine),
+                self._return_zero(self._work, home_quarantine),
+                self._return_zero(self._other_location, home_quarantine)
+            )
+
+        if not_allowed_work:
+            self._work = self._return_zero(self._work, not_allowed_work)
+
+        if not_allowed_other:
+            self._other_location = self._return_zero(self._other_location, not_allowed_other)
+
+        if not_allowed_school:
+            self._school = self._return_zero(self._school, not_allowed_school)
+
+        contact_school, contact_work, contact_other = (
+            self._compute_contact(self._school, prop_school, sd_school),
+            self._compute_contact(self._work, prop_work, sd_work),
+            self._compute_contact(self._other_location, prop_other, sd_other)
+        )
+
+        contact = contact_school + contact_work + contact_other
+
+        if home:
+            contact += self._home
+
+        return contact
+
+
+if __name__ == '__main__':
+    contact_matrix = ContactMatrix()
+    print(contact_matrix.get_contact())
