086a6b3a315c3db01f7559d438c5f3f16cb2ee88
Dave <d.e.magno13@gmail.com>
2021-11-23 12:16:49 +0800

    Fix: change path to data folder

--- epcontrol/utils.py
+++ epcontrol/utils.py
@@ -18,6 +18,7 @@
 #ddis: second derivative of the infected counts
 import csv
 from typing import List, Optional
+from pathlib import Path
 
 import numpy as np
 
@@ -35,13 +36,11 @@ def find_peaks(inf_normalized, dis, ddis, threshold):
     return peaks
 
 
-def export_states(states: List[np.ndarray], filename: Optional[str] = None) -> None:
+def export_states(states: List[np.ndarray], filename: str = 'states.csv') -> None:
 
     # FIXME: add path to destination folder: 'data/processed/states'
+    states_path = lambda filename: Path.joinpath(Path.cwd(), 'data/results', filename)
 
-    if filename is None:
-        filename = 'states.csv'
-
-    with open(filename, 'w') as f:
+    with open(states_path(filename), 'w') as f:
         write = csv.writer(f)
         write.writerow(states)
